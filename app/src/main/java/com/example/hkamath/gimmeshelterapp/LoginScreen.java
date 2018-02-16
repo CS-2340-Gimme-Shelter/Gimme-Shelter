3c76d1d6c3dc4482f9c518683e864ff0ed762083
Carl Schriever <crschriever@gmail.com>
2018-02-16 14:31:51 -0500

    Renamed loginScreen to LoginScreen @Ryan

--- app/src/main/java/com/example/hkamath/gimmeshelterapp/LoginScreen.java
+++ app/src/main/java/com/example/hkamath/gimmeshelterapp/LoginScreen.java
@@ -4,41 +4,26 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
-
 /**
  * A login screen that offers login via username/password.
  */
-public class loginScreen extends AppCompatActivity {
+public class LoginScreen extends AppCompatActivity {
 
     /**
      * Id to identity READ_CONTACTS permission request.
@@ -250,7 +235,7 @@ public class loginScreen extends AppCompatActivity {
             showProgress(false);
 
             if (success) {
-                Intent myIntent = new Intent(loginScreen.this,
+                Intent myIntent = new Intent(LoginScreen.this,
                         HomePage.class);
                 startActivity(myIntent);
             } else {
--- app/src/main/java/com/example/hkamath/gimmeshelterapp/MainPage.java
+++ app/src/main/java/com/example/hkamath/gimmeshelterapp/MainPage.java
@@ -2,9 +2,7 @@ package com.example.hkamath.gimmeshelterapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.app.Activity;
 import android.content.Intent;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -28,7 +26,7 @@ public class MainPage extends AppCompatActivity {
 
                 // Start NewActivity.class
                 Intent myIntent = new Intent(MainPage.this,
-                        loginScreen.class);
+                        LoginScreen.class);
                 startActivity(myIntent);
             }
         });
