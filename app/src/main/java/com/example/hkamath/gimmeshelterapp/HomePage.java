27c0bd40ee12751d6cfca0812c5e077bd708eafe
Carl Schriever <crschriever@gmail.com>
2018-02-16 14:08:28 -0500

    Added logout functionality

--- app/src/main/java/com/example/hkamath/gimmeshelterapp/HomePage.java
+++ app/src/main/java/com/example/hkamath/gimmeshelterapp/HomePage.java
@@ -1,13 +1,32 @@
 package com.example.hkamath.gimmeshelterapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomePage extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        // Locate the button in activity_main.xml
+        button = (Button) findViewById(R.id.logout_button);
+
+        // Capture button clicks
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                // Start NewActivity.class
+                Intent myIntent = new Intent(HomePage.this,
+                        MainPage.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
