a3fe9714ef1c31bd83afd544bdb766eec5f47619
Harish Kamath <31756065+harish-kamath@users.noreply.github.com>
2018-04-10 13:22:32 -0400

    Update HarishUnitTest.java

--- app/src/test/java/com/example/hkamath/gimmeshelterapp/HarishUnitTest.java
+++ app/src/test/java/com/example/hkamath/gimmeshelterapp/HarishUnitTest.java
@@ -16,3 +16,32 @@ public void onPostExecute(Boolean success, String error) {
         }
     }
 */
+
+package com.example.hkamath.gimmeshelterapp;
+import com.example.hkamath.gimmeshelterapp.model.Gender;
+import com.example.hkamath.gimmeshelterapp.model.User;
+
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+/**
+ * Created by uday on 4/9/18.
+ */
+
+public class HarishUnitTest {
+
+    /**
+     * Tests the PostExecute function described above.
+     */
+    @Test
+    public void userEquals() throws Exception {
+        User a = new User("Uday", "Patil", "udpatil", "password", Gender.MALE, new Date(1998, 5, 26), false, null);
+        User b = new User("Uday", "Patil", "udpatil", "password", Gender.MALE, new Date(1998, 5, 26), false, null);
+        String c = "udpatil";
+        assertEquals(a, b);
+        assertEquals(a, a);
+        assertNotEquals(a, c);
+    }
+}
