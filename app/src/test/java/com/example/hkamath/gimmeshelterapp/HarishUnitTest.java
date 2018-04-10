957060cec5cb38421084326fc5ac63deaea67fe1
Harish Kamath <31756065+harish-kamath@users.noreply.github.com>
2018-04-10 15:29:51 -0400

    Update HarishUnitTest.java

--- app/src/test/java/com/example/hkamath/gimmeshelterapp/HarishUnitTest.java
+++ app/src/test/java/com/example/hkamath/gimmeshelterapp/HarishUnitTest.java
@@ -1,25 +1,24 @@
 /*
-public void onPostExecute(Boolean success, String error) {
-        mAuthTask = null;
-
-        if (success.booleanValue()) {
-            Intent myIntent = new Intent(LoginScreen.this,
-                    HomePage.class);
-            // Can't hit back button and come here
-            myIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(myIntent);
-            finish();
-        } else {
-            mEmailView.setError(error);
-            mEmailView.requestFocus();
-            showProgress(false);
+ public static boolean giveShelterGuest(Shelter shelter, User user, int num) {
+        long capacity = shelter.getCapacity();
+        int numReserved = (int) shelter.getVisitors().values().stream().mapToInt(Number::intValue).sum();
+        if (capacity < numReserved + num) {
+            return false;
         }
+        database.getReference("Shelter").child(shelter.getUniqueKey() + "").child("visitors")
+                .child(user.getFirebaseUser().getUid()).setValue(num);
+        database.getReference("User").child(user.getFirebaseUser().getUid()).child("bedRequestedShelter").setValue(shelter.getUniqueKey());
+        user.setBedRequestedShelter(shelter.getUniqueKey());
+        shelter.getVisitors().put(user.getFirebaseUser().getUid(), num);
+
+        return true;
     }
 */
 
 package com.example.hkamath.gimmeshelterapp;
-import com.example.hkamath.gimmeshelterapp.model.Gender;
+import com.example.hkamath.gimmeshelterapp.model.Shelter;
 import com.example.hkamath.gimmeshelterapp.model.User;
+import com.example.hkamath.gimmeshelterapp.model.APIUtil;
 
 import org.junit.Test;
 
@@ -33,15 +32,19 @@ import static org.junit.Assert.*;
 public class HarishUnitTest {
 
     /**
-     * Tests the PostExecute function described above.
+     * Tests the giveShelterGuest function described above, in the APIUtil.java class
      */
     @Test
-    public void userEquals() throws Exception {
-        User a = new User("Uday", "Patil", "udpatil", "password", Gender.MALE, new Date(1998, 5, 26), false, null);
-        User b = new User("Uday", "Patil", "udpatil", "password", Gender.MALE, new Date(1998, 5, 26), false, null);
-        String c = "udpatil";
-        assertEquals(a, b);
-        assertEquals(a, a);
-        assertNotEquals(a, c);
+    public void giveShelterGuestTest() throws Exception {
+        Shelter s1 = new Shelter("address",2,10,10,"number",null,"s1","None",1,null);
+        User u1 = new User("u1","last","user1","pass1",null,null,false,null);
+        User u2 = new User("u2","last","user1","pass1",null,null,false,null);
+        User u3 = new User("u3","last","user1","pass1",null,null,false,null);
+
+        assertEquals(true,APIUtil.giveShelterGuest(s1,u1,1));
+        assertEquals(true,APIUtil.giveShelterGuest(s1,u2,1));
+        assertEquals(false,APIUtil.giveShelterGuest(s1,u3,1));
+        s1.setCapacity(3);
+        assertEquals(true,APIUtil.giveShelterGuest(s1,u3,1));
     }
 }
